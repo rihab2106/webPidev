df95762a82970d3923860bbb0db14a8bff76217c
Syrine Amami <75095786+syrine20@users.noreply.github.com>
2022-04-10 23:41:04 +0100

    sybf

--- src/Controller/TeamsController.php
+++ src/Controller/TeamsController.php
@@ -35,6 +35,17 @@ class TeamsController extends AbstractController
     }
 
     /**
+     * @Route("/displayTeamsFront", name="displayTeamsFront")
+     */
+    public function displayFront()
+    {
+        $rep=$this->getDoctrine()->getRepository(Teams::class);
+        return $this->render('teams/displayTeamsFront.html.twig', [
+            'teams' => $rep->findAll()
+        ]);
+    }
+
+    /**
      * @Route("/addTeams", name="addTeams")
      */
     public function add(Request $request)
@@ -54,6 +65,29 @@ class TeamsController extends AbstractController
         }
         return $this->render('teams/addTeams.html.twig', [
             "form"=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/addTeamsFront", name="addTeamsFront")
+     */
+    public function addFront(Request $request)
+    {
+        $mng=$this->getDoctrine()->getManager();
+        $c=new Teams();
+        $formf=$this->createForm(TeamsFormType::class,$c);
+        $formf->add("Add Team",SubmitType::class);
+        $formf->handleRequest($request);
+        if($formf->isSubmitted() && $formf->isValid())
+        {
+            $c=$formf->getData();
+            $mng->persist($c);
+            $mng->flush();
+            return $this->redirectToRoute("displayTeamsFront");
+
+        }
+        return $this->render('teams/addTeamsFront.html.twig', [
+            "formf"=>$formf->createView()
         ]);
     }
 
@@ -80,6 +114,30 @@ class TeamsController extends AbstractController
         ]);
     }
 
+
+    /**
+     * @Route("/updateTeamsFront/{id}", name="updateTeamsFront")
+     */
+    public function updateFront($id,Request $request)
+    {
+        $mng=$this->getDoctrine()->getManager();
+        $rep=$this->getDoctrine()->getRepository(Teams::class);
+        $c=$rep->find($id);
+        $formf=$this->createForm(TeamsFormType::class,$c);
+        $formf->add("Update Teams",SubmitType::class);
+        $formf->handleRequest($request);
+        if($formf->isSubmitted() && $formf->isValid())
+        {
+            $c=$formf->getData();
+            $mng->flush();
+            return $this->redirectToRoute("displayTeamsFront");
+
+        }
+        return $this->render('teams/addTeamsFront.html.twig', [
+            "formf"=>$formf->createView()
+        ]);
+    }
+
     /**
      * @Route("/deleteTeams/{id}", name="deleteTeams")
      */
@@ -90,6 +148,20 @@ class TeamsController extends AbstractController
         $mng->remove($rep->find($id));
         $mng->flush();
         return $this->redirectToRoute("displayTeams");
+
+    }
+
+
+    /**
+     * @Route("/deleteTeamsFront/{id}", name="deleteTeamsFront")
+     */
+    public function deleteFront($id)
+    {
+        $rep=$this->getDoctrine()->getRepository(Teams::class);
+        $mng=$this->getDoctrine()->getManager();
+        $mng->remove($rep->find($id));
+        $mng->flush();
+        return $this->redirectToRoute("displayTeamsFront");
 
     }
 
